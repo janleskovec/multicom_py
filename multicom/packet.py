bd385c5c7b0ee89729f39da7cb0b20417992f854
Jan Leskovec <janleskovec1@gmail.com>
2022-03-04 11:44:58 +0100

    update definition

--- multicom/packet.py
+++ multicom/packet.py
@@ -6,7 +6,7 @@ class PacketType(Enum):
     DISCOVERY_HELO  = 1 # discovery reply packet
     PING            = 2 # echo
     GET             = 3 # does not use nonce (callback called again on re-transmit)
-    GET_REPLY       = 4 # reply msg after get
+    GET_REPLY       = 4 # uses random nonce (does not prevent dupicate callbacks)
     SEND            = 5 # uses nonce (ensures callback only gets called once + order)
     POST            = 6 # uses nonce + sends ack (ensures callback only gets called once + order)
     ACK             = 7 # used to reply after post (contains session id and latest nonce)
